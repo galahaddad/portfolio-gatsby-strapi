4a3480f3c5902e59ce09f26795306bd7c4ac5e5a
Ralph Haddad <ralphhaddad777@gmail.com>
2020-08-29 16:10:26 -0700

    Added submodule backend

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -2,6 +2,7 @@ import React from "react"
 import Layout from "../components/Layout"
 import { graphql } from "gatsby"
 import Blogs from "../components/Blogs"
+import SEO from "../components/SEO"
 // ...GatsbyImageSharpFluid
 
 export const query = graphql`
@@ -32,6 +33,7 @@ const Blog = ({
   },
 }) => {
   return <Layout>
+    <SEO title="Home" description="This is Ralph's home page"/>
     <section className="blog-page">
       <Blogs blogs={blogs} title="blogs"/>
     </section>
